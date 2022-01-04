d22ac7550d4d6f908f88137b3fd659c049f7cc9d
Yendry <yendrycastanera@gmail.com>
2022-01-03 20:50:58 -0500

    adding react + data rest project

---
+++ my-react-and-spring-data-rest/src/main/java/org/example/myreactandspringdatarest/MyReactAndSpringDataRestApplication.java
@@ -0,0 +1,13 @@
+package org.example.myreactandspringdatarest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyReactAndSpringDataRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyReactAndSpringDataRestApplication.class, args);
+    }
+
+}
---
+++ my-react-and-spring-data-rest/src/test/java/org/example/myreactandspringdatarest/MyReactAndSpringDataRestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.myreactandspringdatarest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyReactAndSpringDataRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
